4626e236a2fee06ee79cbfda6d0e00cd341395bd
Dreamlone <mik_sar@mail.ru>
2023-10-19 17:07:22 +0300

    add ability to load data from file

--- estaty/repository/labels.py
+++ estaty/repository/labels.py
@@ -1,18 +1,20 @@
 from estaty.analysis.stages.area import AreaAnalysisStage
 from estaty.analysis.stages.extend_clarification import \
     ExtendClarificationAnalysisStage
-from estaty.data_source.load.gbif_vector import LoadGBIFLocallyStage
+from estaty.data_source.load.vector_csv import LoadCsvVectorStage
 from estaty.data_source.load.landsat_raster import NDVILandsatLocallyStage
 from estaty.data_source.load.osm_vector import LoadOSMStage
+from estaty.merge.vector_merge import VectorDataMergingStage
 from estaty.preprocessing.stages.project import CommonProjectionStage
 from estaty.analysis.stages.distance import DistanceAnalysisStage
 from estaty.stages import DummyStage
 
 
 DATA_SOURCE_POOL_BY_NAME = {'osm': [LoadOSMStage],
-                            'gbif_local': [LoadGBIFLocallyStage],
+                            'csv': [LoadCsvVectorStage],
                             'ndvi_local': [NDVILandsatLocallyStage],
                             'reproject': CommonProjectionStage,
+                            'vector': VectorDataMergingStage,
                             'distance': DistanceAnalysisStage,
                             'area': AreaAnalysisStage,
                             'extend_clarification': ExtendClarificationAnalysisStage,
